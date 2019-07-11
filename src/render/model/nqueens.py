4abf02ecd293a6845313ea3b54d6fd9fd096e0d0
Nicholas <nicholas.janulewicz@gmail.com>
2019-07-10 20:39:32 -0500

    simulated_annealing updated
    Simplified, as good as before although its still not optimal

--- src/render/model/nqueens.py
+++ src/render/model/nqueens.py
@@ -130,6 +130,24 @@ def expand(board, nqueens):
 
     return move_list
 
+    successorList = []
+
+
+def generate_neighbor(board, move_list):
+
+    neighbors = []
+
+    for move in move_list:
+        changed_board = board.copy()
+        changed_board[move[0]] = move[move[1]]
+        neighbors.append(changed_board)
+
+    for state in neighbors:
+        if evaluate_state(state) > evaluate_state(board):
+            board = state
+
+    return board
+
 
 def random_search(board):
     """
@@ -208,19 +226,12 @@ def hill_climbing(board, nqueens):
 
         move_list = expand(board, nqueens)
 
-        if move_list is None:
-            print('no neighbors')
-            break
-        else:
-            for move in move_list:
-                changed_board = board.copy()
-                changed_board[move[0]] = move[move[1]]
-                neighbors.append(changed_board)
+        neighbor = generate_neighbor(board, move_list)
 
-        for state in neighbors:
-            if state not in explored:
-                if evaluate_state(state) > evaluate_state(board):
-                    board = state
+        if neighbor not in explored:
+            board = neighbor
+        else:
+            board = random_restart(board)
 
         move_list.clear()
         neighbors.clear()
@@ -231,123 +242,9 @@ def hill_climbing(board, nqueens):
     print_board(board)
 
 
-def schedule(k = 20, lam = 0.005, limit = 100):
-    """
-    One possible schedule function for simulated annealing uses pythons functional paradigm
-    """
+def schedule(k=20, lam=0.005, limit=100):
+    """One possible schedule function for simulated annealing"""
     return lambda t: (k * math.exp(-lam * t) if t < limit else 0)
-
-
-def simulated_annealing(board, nqueens):
-    """
-    Iteration of hill climbing that aims to "structure" random walking.
-    The innermost loop of the simulated-annealing algorithm is quite similar to hill climbing.
-    Instead of picking the best move, however, it picks a random move. If the move improves the situation,
-    it is always accepted. Otherwise, the algorithm accepts the move with some probability less than 1 (Artificial Intelligence p. 125).
-    :param nqueens: The number of queens in the problem, also affects board size.
-    :param board: list/array representation of columns and the row of the queen on that column
-    :return: A solution if found or, the final state in a human readable format.
-    """
-
-    """
-    Python implamentation from the text's github
-    def simulated_annealing(problem, schedule=exp_schedule()):
-    current = Node(problem.initial)
-    for t in range(sys.maxsize):
-        T = schedule(t)
-        if T == 0:
-            return current.state
-        neighbors = current.expand(problem)
-        if not neighbors:
-            return current.state
-        next_choice = random.choice(neighbors)
-        delta_e = problem.value(next_choice.state) - problem.value(current.state)
-        if delta_e > 0 or probability(math.exp(delta_e / T)):
-            current = next_choice
-    pseudocode from the text (Artificial Intelligence p.126)
-        function SIMULATED-ANNEALING(problem, schedule) returns a solution state
-            inputs: problem, a problem
-                    schedule, a mapping from time to “temperature”
-            current ← MAKE-NODE(problem.INITIAL-STATE)
-            for t = 1 to ∞ do
-                T ← schedule(t)
-                if T = 0 then return current
-                next ← a randomly selected successor of current
-                ΔE ← next.VALUE – current.VALUE
-                if ΔE > 0 then current ← next
-                else current ← next only with probability eΔE/T
-    available functions
-    in_conflict_with_another_queen(row, column, board)
-    in_conflict(column, row, other_column, other_row)
-    count_conflicts(board)
-    evaluate_state(board)
-    print_state(board)
-    """
-
-    # Variables
-    annealing = schedule()
-    optimum = (len(board) - 1) * len(board) / 2
-    i = 0
-    explored = []
-    neighbors = []
-
-    # Logic
-    while evaluate_state(board) != optimum:
-        print('iteration ' + str(i) + ': evaluation = ' + str(evaluate_state(board))
-              + ' conflicts ' + str(count_conflicts(board))) # + ' Schedule ' + str(t))
-        if i == LIMIT:  # Give up after 1000 tries.
-            break
-
-        i += 1
-        explored.append(board)
-
-        # for x in range(sys.maxsize):
-
-        if board in explored:  # if we get stuck in a loop step left in a place or 2
-            for column, row in enumerate(board):
-                if column / random.randint(1, 2) == 0:
-                    board[column] = board[column] - 1
-
-        for x in range(0, LIMIT):
-            t = annealing(x)
-            if t == 0:
-                if evaluate_state(board) == optimum:
-                    print('Solved puzzle!')
-                    print_board(board)
-                    return
-                else:
-                    break
-
-            move_list = expand(board, nqueens)
-
-            if move_list is None:
-                print('no neighbors')
-                break
-            else:
-                for move in move_list:
-                    changed_board = board.copy()
-                    changed_board[move[0]] = move[1]
-                    neighbors.append(changed_board)
-
-            for state in neighbors:
-                if state not in explored:
-                    if evaluate_state(state) > evaluate_state(board):
-                        new_board = state
-
-            delta = evaluate_state(new_board) - evaluate_state(board)
-
-            if delta > 0:
-                board = new_board.copy()
-            elif probability(delta, t):
-                board = new_board.copy()
-
-            move_list.clear()
-            neighbors.clear()
-
-    if evaluate_state(board) == optimum:
-        print('Solved puzzle!')
-    print('Final state is:')
-    print_board(board)
 
 
 def probability(delta, t):
@@ -360,6 +257,56 @@ def probability(delta, t):
         return True
     else:
         return False
+
+
+def simulated_annealing(board, nqueens):
+    """
+    TODO: gets stuck on shoulders, figure out sideways moves.
+    Iteration of hill climbing that aims to "structure" random walking.
+    The innermost loop of the simulated-annealing algorithm is quite similar to hill climbing.
+    Instead of picking the best move, however, it picks a random move. If the move improves the situation,
+    it is always accepted. Otherwise, the algorithm accepts the move with some probability less than 1 (Artificial Intelligence p. 125).
+    :param nqueens: The number of queens in the problem, also affects board size.
+    :param board: list/array representation of columns and the row of the queen on that column
+    :return: A solution if found or, the final state in a human readable format.
+    """
+
+    # Variables
+    annealing = schedule()
+    optimum = (len(board) - 1) * len(board) / 2
+    iterator = 0
+
+    # Logic
+    while evaluate_state(board) != optimum:
+
+        print('iteration ' + str(iterator) + ': evaluation = ' + str(evaluate_state(board))
+              + ' conflicts ' + str(count_conflicts(board)))  # + ' Schedule ' + str(t))
+
+        if iterator == LIMIT:  # Give up after 1000 tries.
+            break
+
+        iterator += 1
+        new_board = board.copy()
+
+        for x in range(sys.maxsize):
+            t = annealing(x)
+            if t == 0:
+                break
+
+            for column, row in enumerate(board):  # For each column, place the queen in a random row
+                new_board[column] = random.randint(0, len(board) - 1)
+
+            delta = evaluate_state(new_board) - evaluate_state(board)
+
+            if delta > 0 or probability(delta, t):
+                board = new_board.copy()
+
+    if evaluate_state(board) == optimum:
+        print('Solved puzzle!')
+    print('Final state is:')
+    print_board(board)
+
+
 
 
 def reproduce(x, y):
